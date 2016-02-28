2144ce48ab32505276e96e659cfbceff3934798e
abbasgh12345 <abbasgh91@gmail.com>
2016-02-29 03:29:24 +0330

    Create fosh.lua

---
+++ plugins/fosh.lua
@@ -0,0 +1,19 @@
+-- https://github.com/amirhmz/
+-- https://telegram.me/AmirDark/
+do
+
+function run(msg, matches)
+  return "دیوث😐 " .. matches[1]
+end
+
+return {
+    patterns = {
+    "^فحش بده به (.*)$",
+    "^[Ff]osh [Bb]ede [Bb]e (.*)$",
+  }, 
+  run = run 
+}
+
+end
+-- https://github.com/amirhmz/
+-- https://telegram.me/AmirDark/
